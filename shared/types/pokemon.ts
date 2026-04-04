a4faed2b1ac665d21af6efe0dc489b786abb8048
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-04-04 03:23:11 +0100

    feat(types): add Pokemon TypeScript interfaces

---
+++ shared/types/pokemon.ts
@@ -0,0 +1,39 @@
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: PokemonType[];
+  sprites: {
+    front_default: string;
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
+  stats: Array<{
+    base_stat: number;
+    stat: { name: string };
+  }>;
+}
+
+export interface PokemonListResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonListItem[];
+}
